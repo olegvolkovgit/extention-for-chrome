429b7b43168184c7ce313d7781373764237d9d9b
echo_54 <olegvolkov.post@gmail.com>
2018-11-18 03:13:32 +0200

    Add files via upload

--- code.js
+++ code.js
@@ -1,33 +1,45 @@
 window.onload = function() {  
-			var storage = window.localStorage;  // ----------------caching path
+/*
+this block (7-24 row) is searching for any content editable elements 
+and change needed words from local storage in case of its detection
+with switch construction
+*/ 
+ var storage = window.localStorage;  
 
-				storage.setItem('Cat','Dog');	//======
-				storage.setItem('Foo','bar');	//===== local sorage filling
-				storage.setItem('Helo','hello');//===== 
-				storage.setItem('heldp','help');//======
+  storage.setItem('Cat', 'Dog');
+  storage.setItem('Foo', 'bar');	
+  storage.setItem('Helo', 'hello');
+  storage.setItem('heldp', 'help');
 
-		var echo = function (input) {//	------------------------------ operational function for
-			var elem = input.target; //------------------------------- HTML 5`s contentEditable elements
-			if (elem.isContentEditable == true) { //------------------ checks on contentedit ability
-	switch(elem.innerHTML){// ---------------------------------------- switch construction for needed words
-		case 'Cat': elem.innerHTML= storage.getItem('Cat'); break;
-		case 'Foo': elem.innerHTML= storage.getItem('Foo'); break;
-		case 'Helo': elem.innerHTML= storage.getItem('Helo'); break;
-		case 'heldp': elem.innerHTML= storage.getItem('heldp'); break;
-				}
-		}
+ var echo = function (input) {
+  var elem = input.target; 
+
+  if (elem.isContentEditable == true) {
+   switch( elem.innerHTML ) {
+	case 'Cat': elem.innerHTML = storage.getItem('Cat'); break;
+	case 'Foo': elem.innerHTML = storage.getItem('Foo'); break;
+	case 'Helo': elem.innerHTML = storage.getItem('Helo'); break;
+	case 'heldp': elem.innerHTML = storage.getItem('heldp'); break;
+	  }
+	 }
+	
 	}
-	document.addEventListener('input',echo); // event listener for all contenteditable elements of document
-
-		var inEcho = function (change) {// --------------------------- operational function for input`s event listener
-			var wordAim = change.target.value;//-------------------- assign editable input element				
-			if (wordAim.includes(" ")) {// ------------------------- space checking
-				var changedWord = wordAim.replace(/Cat/g,'Dog') 
-										 .replace(/Foo/g,'bar')     
-										 .replace(/Helo/g,'hello')//------ Replacing of included woords
-										 .replace(/heldp/g,'help')
-						    	  change.target.value = changedWord; //--- set replaced word instead
-										}
-									  }
-	document.addEventListener('input',inEcho); // event listener for all 'input' elements of document
+ document.addEventListener('input', echo); 
+/*
+this block (32-44) listen to changes in all page inputs and change user`s words
+to needed examples after entering space button
+*/
+ var inEcho = function(change) {
+  var wordAim = change.target.value;
+ 
+  if (wordAim.includes(" ")) {
+  var changedWord = wordAim.replace(/Cat/g, 'Dog') 
+						   .replace(/Foo/g, 'bar')     
+						   .replace(/Helo/g, 'hello')
+						   .replace(/heldp/g, 'help')
+  change.target.value = changedWord;
+	}
+ 
+   }
+ document.addEventListener('input', inEcho);
 }
